43ee6b50f72f4b1a3f49032923bbe8f05aa784c0
Anushree <anushreebhatt357@gmail.com>
2025-04-04 11:23:39 -0400

    Initialized project

---
+++ src/test/java/com/practice/springboot/JPA_10_steps/Jpa10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.JPA_10_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
